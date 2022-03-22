04bcd3bed00e8df743b98c34b7ee8908bd9c1c2d
bogdankornij <bogdankornij656@gmail.com>
2022-03-22 15:27:00 +0200

    added readme and install 1 step

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Installing pmta on a server</h1>
+
+<h3>How to run</h3>
+<p>U need to connect to your new server and run next:
+<code>curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/install.sh | bash</code>
+</p>
+
--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-echo 'hello server!';
+#!/usr/bin/env bash
+#3. create mailpass.txt
+#4. create domains.txt
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+yum check-update
+yum update
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/1-Start.sh | bash

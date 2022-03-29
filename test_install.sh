53e625430bad2b4c522c86d496e75f70e796f8a7
bogdankornij <bogdankornij656@gmail.com>
2022-03-29 15:26:10 +0300

    restart

--- test_install.sh
+++ test_install.sh
@@ -27,6 +27,9 @@ curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/9-
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/10-PMTA-Install.sh | bash
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/test_set_config.sh | bash
 
+service pmta reload
+service pmta restart
+
 echo ' ';
 echo '|\  ||  /|';
 echo '--  ||  --';

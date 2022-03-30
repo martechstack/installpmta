1a07c456cf278b944f7a6e799e8984afcf7cb5c9
bogdankornij <bogdankornij656@gmail.com>
2022-03-30 10:08:32 +0300

    exit

--- test_install1.sh
+++ test_install1.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/change_port.sh | bash
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
-yum check-update
-yum update
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/test_install_mirror.sh | bash
+#yum check-update
+#yum update
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/1-Start.sh | bash
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/2-LAMP.sh | bash
 php set_domains.php

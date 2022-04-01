bb0926d770f46930e7926e098465c8a763b1effd
yuriilevkovych <yurii.levkovich@gmail.com>
2022-04-01 18:08:05 +0300

    exit

--- test_install1.sh
+++ test_install1.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/change_port.sh | bash
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/test_install_mirror.sh | bash
 #yum check-update
 #yum update
@@ -16,6 +15,8 @@ curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/7-
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/8-Zip.sh | bash
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/9-PMTA-Config.sh | bash
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/10-PMTA-Install.sh | bash
+
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/test_change_server_port.sh | bash
 curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/test_set_config.sh | bash
 
 service pmta reload

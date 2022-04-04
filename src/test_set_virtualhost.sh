5d4674ce1b9c97689a267f9d85f711de16a29629
yuriilevkovych <yurii.levkovich@gmail.com>
2022-04-04 21:31:13 +0300

    pattern

--- src/test_set_virtualhost.sh
+++ src/test_set_virtualhost.sh
@@ -1,8 +1,27 @@
-#!/usr/bin/env bash
 
-sed -i 's/#Port 22/Port 1122/g' /etc/ssh/sshd_config
+cat <<'EOF' >$HOME/set_virtualhost.php
+<?php
+
+$path_to_file = "/etc/pmta/virtualhost.txt";
+$pattern = '/\<domain \*\>\n\s.*\<\/domain\>/i';
+$replacement = '<domain $tmobile>
+        max-msg-rate 1/s
+    </domain>
+    <domain $verizon>
+        max-msg-rate 1/s
+    </domain>
+    <domain $att>
+        max-msg-rate 1/s
+    </domain>';
+$content = file_get_contents($path_to_file);
+
+$content = preg_replace($pattern, $replacement, $content);
+
+file_put_contents($path_to_file, $content);
+EOF
+
+php $HOME/set_virtualhost.php
 
 
-echo '/etc/pmta/virtualhost.txt has set!';
-echo '||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||';echo '';
-echo '';
+
+

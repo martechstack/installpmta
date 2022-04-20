a5c756bb16f0ce849d5134c982b7c324a6305319
yuriilevkovych <yurii.levkovich@gmail.com>
2022-04-20 16:08:21 +0300

    ma

--- make_config.sh
+++ make_config.sh
@@ -2,19 +2,6 @@
 
 cat <<'EOF' >$HOME/config.json
 
-
-
-
-#{
-#  "domain": "",
-#  "server_id": "",
-#  "server_ip": "",
-#  "server_pass": "",
-#  "api_key": "",
-#  "plex_user": "",
-#  "plex_pass": "",
-#  "plex_db": ""
-#}
 EOF
 
 echo 'Now input: nano config.json'

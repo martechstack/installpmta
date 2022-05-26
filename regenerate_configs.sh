d67a05ba86e1c3e004d204fb49f6bc17ce8c5dbd
yuriilevkovych <yurii.levkovich@gmail.com>
2022-05-26 19:10:25 +0300

    gitignore

---
+++ regenerate_configs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/9-PMTA-Config.sh | bash
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/10-PMTA-Install.sh | bash
+
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/set_pmta_config.sh | bash
+curl -s https://raw.githubusercontent.com/martechstack/installpmta/master/src/set_virtualhost2.sh | bash
+
+service pmta reload
+service pmta restart
+service pmta status
+
+echo ' ';
+echo '|\  ||  /|';
+echo '--  ||  --';
+echo '|/__/\__\|';
+echo '    \/';

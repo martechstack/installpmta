504ef9f54d290e70bf3e71e65416ca491bf4bb29
yuriilevkovych <yurii.levkovich@gmail.com>
2022-06-29 16:49:53 +0300

    readme

--- src/change_server_port_1122.sh
+++ src/change_server_port_1122.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sed -i 's/#Port 22/Port 1122/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd.service
+systemctl restart sshd.service
 echo 'Port changed!'
 echo '||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||';echo '';
